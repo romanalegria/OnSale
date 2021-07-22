f518b762bb229f310f80bd7ebecaa2f723ba3210
Emilio Roman Alegria <eranet1973@hotmail.com>
2021-07-22 18:03:30 -0400

    add cetegories and product

---
+++ OnSale.Common/Entities/Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnSale.Common.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "The field {0} must contain less than {1} characteres.")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Image")]
+        public Guid ImageId { get; set; }
+
+        //TODO: Pending to put the correct paths
+        [Display(Name = "Image")]
+        public string ImageFullPath => ImageId == Guid.Empty
+            ? $"https://localhost:44390/images/noimage.png"
+            : $"https://onsale.blob.core.windows.net/categories/{ImageId}";
+
+    }
+}
--- OnSale.Web/Data/DataContext.cs
+++ OnSale.Web/Data/DataContext.cs
@@ -9,9 +9,12 @@ namespace OnSale.Web.Data
         {
         }
 
+        public DbSet<Category> Categories { get; set; }
         public DbSet<City> Cities { get; set; }
         public DbSet<Country> Countries { get; set; }
         public DbSet<Department> Departments { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
 
 
 
@@ -19,10 +22,13 @@ namespace OnSale.Web.Data
         {
             base.OnModelCreating(modelBuilder);
 
+            modelBuilder.Entity<Category>()
+            .HasIndex(t => t.Name)
+            .IsUnique();
+
             modelBuilder.Entity<City>()
            .HasIndex(t => t.Name)
            .IsUnique();
-
 
             modelBuilder.Entity<Country>()
                 .HasIndex(t => t.Name)
@@ -31,6 +37,11 @@ namespace OnSale.Web.Data
             modelBuilder.Entity<Department>()
             .HasIndex(t => t.Name)
             .IsUnique();
+
+            modelBuilder.Entity<Product>()
+            .HasIndex(t => t.Name)
+            .IsUnique();
+
 
         }
 
